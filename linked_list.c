2b77387331468b1f7d79aa456885673a5d4a0bba
1500842 <e1500842@shell.vamk.fi>
2018-10-23 11:03:46 +0300

    jee

--- linked_list.c
+++ linked_list.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 #include "linked_list.h"
+
+int add_to_list(linked_list *ll, char *s)
+{
+	
+	
+}
+
+int display_item(linked_list *ll)
+{
+	
+}
+int display_list(linked_list *ll)
+{
+	
+}
+linked_list * search_from_list(linked_list *ll, char *s)
+{
+	
+}
+int delete_from_list(linked_list *ll, int index)
+{
+	
+}
